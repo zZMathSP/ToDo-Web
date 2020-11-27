2e282a904efd14255ad16e727dfae2ceb632f8ed
Matheus Silva <matheus.silva@ssys.com.br>
2020-11-27 16:08:19 -0400

    Adiconando templates, correção no dockerfile

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -3,11 +3,7 @@ import os
 from werkzeug.utils import secure_filename
 from flask import (
     Flask,
-    jsonify,
-    send_from_directory,
-    request,
-    redirect,
-    url_for
+    render_template
 )
 from flask_sqlalchemy import SQLAlchemy
 
@@ -29,30 +25,12 @@ class User(db.Model):
 
 
 @app.route("/")
-def hello_world():
-    return jsonify(hello="world in a docker container")
+def index():
+    return render_template('index.html', data=[{
+        'description': 'Todo 1'
+    }, {
+        'description': 'Todo 2'
+    }, {
+        'description': 'Todo 3'
+    }])
 
-
-@app.route("/static/<path:filename>")
-def staticfiles(filename):
-    return send_from_directory(app.config["STATIC_FOLDER"], filename)
-
-
-@app.route("/media/<path:filename>")
-def mediafiles(filename):
-    return send_from_directory(app.config["MEDIA_FOLDER"], filename)
-
-
-@app.route("/upload", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        file = request.files["file"]
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
-    return f"""
-    <!doctype html>
-    <title>upload new File</title>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file><input type=submit value=Upload>
-    </form>
-    """
