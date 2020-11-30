cac7a6b028d9520a05bdc3105025b5e59b897c02
Matheus Silva <matheus.silva@ssys.com.br>
2020-11-30 17:53:04 -0400

    Modificando o crud e melhorando a view

--- services/web/project/routes.py
+++ services/web/project/routes.py
@@ -16,21 +16,65 @@ def create_todo():
     body = {}
     try:
         description = request.get_json()['description']
-        todo = Todo(description=description)
+        list_id = request.get_json()['list_id']
+        todo = Todo(description=description, complete=False, list_id=list_id)
         db.session.add(todo)
         db.session.commit()
+        body['id'] = todo.id
+        body['complete'] = todo.complete
         body['description'] = todo.description
-    except:
-        error = True
+    except():
         db.session.rollback()
+        error = True
+        print(sys.exc_info())
     finally:
         db.session.close()
-        if not error:
-            return jsonify(body)
+    if error:
+        abort(500)
+    else:
+        return jsonify(body)
 
+@app.route('/todos/<todo_id>/set-complete', methods=['POST'])
+def update_todo(todo_id):
+    error = False
+    try:
+        complete = request.get_json()['complete']
+        todo = Todo.query.get(todo_id)
+        print('Todo: ', todo)
+        todo.complete = complete
+        db.session.commit()
+    except():
+        db.session.rollback()
+        error = True
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/todos/<todo_id>/delete', methods=['DELETE'])
+def delete_todo(todo_id):
+    error = False
+    try:
+        #Todo.query.filter_by(id=todo_id).delete()
+        todo = Todo.query.get(todo_id)
+        db.session.delete(todo)
+        db.session.commit()
+    except():
+        db.session.rollback()
+        error = True
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else:
+        return jsonify({'success': True})
 
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
-def set_completed_todos(todo_id):
+def set_completed_todo(todo_id):
+    error = False
     try:
         completed = request.get_json()['completed']
         todo = Todo.query.get(todo_id)
@@ -38,29 +82,28 @@ def set_completed_todos(todo_id):
         db.session.commit()
     except:
         db.session.rollback()
+        error = True
     finally:
         db.session.close()
-    return redirect(url_for('index'))
+    if error:
+        abort(500)
+    else:
+        return '', 200
 
 
-@app.route('/todos/<todo_id>', methods=['DELETE'])
-def delete_todos(todo_id):
-    try:
-        Todo.query.filter_by(id=todo_id).delete()
-        db.session.commit()
-    except:
-        db.session.rollback()
-    finally:
-        db.session.close()
-    return jsonify({'success': True})
+@app.route('/')
+def index():
+    return redirect(url_for('get_list_todos', list_id=1))
 
 
 @app.route('/lists/<list_id>')
 def get_list_todos(list_id):
-    return render_template('index.html',
-                           lists=TodoList.query.all(),
-                           active_list=TodoList.query.get(list_id),
-                           todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+    lists = TodoList.query.all()
+    active_list = TodoList.query.get(list_id)
+    todos = Todo.query.filter_by(list_id=list_id).order_by('id').all()
+
+    return render_template('index.html', todos=todos, lists=lists, active_list=active_list)
+
 
 @app.route('/lists/create', methods=['POST'])
 def create_list():
@@ -105,31 +148,21 @@ def delete_list(list_id):
     else:
         return jsonify({'success': True})
 
-
+        
 @app.route('/lists/<list_id>/set-completed', methods=['POST'])
 def set_completed_list(list_id):
     error = False
-
     try:
         list = TodoList.query.get(list_id)
-
         for todo in list.todos:
             todo.completed = True
-
         db.session.commit()
     except:
         db.session.rollback()
-
         error = True
     finally:
         db.session.close()
-
     if error:
         abort(500)
     else:
         return '', 200
-
-
-@app.route('/')
-def index():
-    return redirect(url_for('get_list_todos', list_id=1))
